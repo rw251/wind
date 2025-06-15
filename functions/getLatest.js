76d9310f1790b7bbcd417ba2486cd0c6700d2ef2
Richard Williams <1234richardwilliams@gmail.com>
2025-06-15 21:52:27 +0100

    Fix

--- functions/getLatest.js
+++ functions/getLatest.js
@@ -1,18 +1,20 @@
 const FEED = {
-  ESO: 'eso',
-  ESO_HISTORIC: 'eso-historic',
-  BMRS_HISTORIC: 'bmrs-historic',
-  BMRS_CURRENT: 'bmrs-current',
+  ESO: "eso",
+  ESO_HISTORIC: "eso-historic",
+  BMRS_HISTORIC: "bmrs-historic",
+  BMRS_CURRENT: "bmrs-current",
 };
+
+// Capacity, Datetime, Wind_Forecast
+
+const NESO_API_ENDPOINT = "https://api.neso.energy/api/3/action/";
 
 let env;
 let data;
 
 async function shouldUpdate(feed) {
   const lastUpdatedString = await env.WIND.get(`LAST-UPDATED-${feed}`);
-  const lastUpdated = lastUpdatedString
-    ? new Date(lastUpdatedString)
-    : new Date(2000, 1, 1);
+  const lastUpdated = lastUpdatedString ? new Date(lastUpdatedString) : new Date(2000, 1, 1);
   console.log(`${feed} last updated on ${lastUpdated}`);
   if (feed === FEED.eso) {
     lastUpdated.setMinutes(lastUpdated.getMinutes() + 140);
@@ -29,14 +31,14 @@ async function shouldUpdate(feed) {
 function xml2json(xml) {
   const arr = [];
   const match = xml.match(/<responseList><item>(.+)<\/item><\/responseList>/);
-  const items = match[1].split('</item><item>').forEach((item) => {
+  const items = match[1].split("</item><item>").forEach((item) => {
     const [, q] = item.match(/<quantity>([^<]+)<\/quantity>/);
     const [, p] = item.match(/<settlementPeriod>([^<]+)<\/settlementPeriod>/);
     const obj = { quantity: +q, period: +p };
-    if (item.indexOf('Wind Offshore') > -1) {
-      obj.type = 'Wind Offshore';
-    } else if (item.indexOf('Wind Onshore') > -1) {
-      obj.type = 'Wind Onshore';
+    if (item.indexOf("Wind Offshore") > -1) {
+      obj.type = "Wind Offshore";
+    } else if (item.indexOf("Wind Onshore") > -1) {
+      obj.type = "Wind Onshore";
     } else {
       return;
     }
@@ -52,14 +54,13 @@ async function getHistoricEsoFeed() {
   oneDayAgo.setDate(oneDayAgo.getDate() - 1);
   const dateFrom = twoDaysAgo.toISOString().substring(0, 10);
   const dateTo = oneDayAgo.toISOString().substring(0, 10);
-  const url = `https://api.nationalgrideso.com/api/3/action/datastore_search_sql?sql=SELECT%20%22Datetime_GMT%22,%22Capacity%22,%22Incentive_forecast%22%20FROM%20%227524ec65-f782-4258-aaf8-5b926c17b966%22%20WHERE%20%22Datetime_GMT%22%20%3E=%20'${dateFrom}T00:00:00.000Z'%20AND%20%22Datetime_GMT%22%20%3C=%20'${dateTo}T23:59:59.999Z'%20ORDER%20BY%20%22_id%22%20ASC%20LIMIT%20100`;
+  const url = `${NESO_API_ENDPOINT}datastore_search_sql?sql=SELECT%20%22Datetime_GMT%22,%22Capacity%22,%22Incentive_forecast%22%20FROM%20%227524ec65-f782-4258-aaf8-5b926c17b966%22%20WHERE%20%22Datetime_GMT%22%20%3E=%20'${dateFrom}T00:00:00.000Z'%20AND%20%22Datetime_GMT%22%20%3C=%20'${dateTo}T23:59:59.999Z'%20ORDER%20BY%20%22_id%22%20ASC%20LIMIT%20100`;
   const data = await fetch(url).then((x) => x.json());
   return data.result.records;
 }
 
 async function getEsoFeed() {
-  const url =
-    'https://api.nationalgrideso.com/api/3/action/datastore_search?resource_id=93c3048e-1dab-4057-a2a9-417540583929&limit=670';
+  const url = `${NESO_API_ENDPOINT}datastore_search?resource_id=93c3048e-1dab-4057-a2a9-417540583929&limit=670&fields=_id,Datetime,Capacity,Wind_Forecast`;
   const data = await fetch(url).then((resp) => resp.json());
   return data.result.records;
 }
@@ -72,7 +73,7 @@ async function getBrmsHistoric() {
   const url = `https://data.elexon.co.uk/bmrs/api/v1/generation/actual/per-type/wind-and-solar?from=${dateFrom}&to=${dateTo}&settlementPeriodFrom=0&settlementPeriodTo=48&format=json`;
   const data = await fetch(url).then((resp) => resp.json());
   return data.data
-    .filter((x) => x.psrType.toLowerCase().indexOf('wind') > -1)
+    .filter((x) => x.psrType.toLowerCase().indexOf("wind") > -1)
     .map((x) => {
       return {
         quantity: x.quantity,
@@ -130,20 +131,25 @@ async function updateFeed(feed) {
 export async function onRequest(context) {
   env = context.env;
 
-  const dataJson = await env.WIND.get('data.json');
-  data = dataJson ? JSON.parse(dataJson) : {};
+  const dataJson = await env.WIND.get("data.json");
+  data = {};
+  if (dataJson) {
+    try {
+      data = JSON.parse(dataJson);
+    } catch (e) {}
+  }
   let updated = await updateFeed(FEED.BMRS_CURRENT);
   updated = (await updateFeed(FEED.BMRS_HISTORIC)) || updated;
   updated = (await updateFeed(FEED.ESO)) || updated;
   updated = (await updateFeed(FEED.ESO_HISTORIC)) || updated;
 
   if (updated) {
-    env.WIND.put('data.json', JSON.stringify(data));
+    await env.WIND.put("data.json", JSON.stringify(data));
   }
 
   return new Response(JSON.stringify(data), {
     headers: {
-      'content-type': 'application/json;charset=UTF-8',
+      "content-type": "application/json;charset=UTF-8",
     },
   });
 }
